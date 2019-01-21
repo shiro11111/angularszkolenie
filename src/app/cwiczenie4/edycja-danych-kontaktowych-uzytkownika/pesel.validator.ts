9c9f62a34da97dd6dd1a58f4384d955f4cf110ae
Małgorzata Szlęzak <ameba01@poczta.fm>
2019-01-21 12:03:25 +0100

    - initial commit

---
+++ src/app/cwiczenie4/edycja-danych-kontaktowych-uzytkownika/pesel.validator.ts
@@ -0,0 +1,25 @@
+import { AbstractControl } from '@angular/forms';
+
+export function ValidatePesel(control: AbstractControl) {
+  if (control.value) {
+    if (control.value.length !== 11) {
+      return { invalidPesel: true };
+    }
+    const pesel = control.value.split('').slice(0, 10);
+
+    const controlNumber = control.value.split('').pop();
+
+    const multipliers = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3];
+
+    let sum = 0;
+    for (let i = 0; i < pesel.length; i++) {
+      sum += +pesel[i] * multipliers[i];
+    }
+
+    const result = (10 - (sum % 10)) % 10;
+    if (+result !== +controlNumber) {
+      return { invalidPesel: true };
+    }
+  }
+  return null;
+}
